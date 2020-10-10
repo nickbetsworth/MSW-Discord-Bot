19f5eea36d4b8cde48d55bbd9949f6e3bc33e6ad
Nick Betsworth <nick.betsworth@gmail.com>
2020-10-10 08:45:44 +0100

    Implemented GetForecast function to grab the forecast for a particular spot.

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
-  var spots mswclient.SpotResults = mswclient.GetSpots("Porthcawl")
+  spots := mswclient.GetSpots("Porthcawl")
   fmt.Println(spots[0].ID)
+
+  forecast := mswclient.GetForecast(spots[0].ID)
+  fmt.Printf("Solid rating: %d\n", forecast[0].SolidRating)
+  fmt.Printf("Faded rating: %d\n", forecast[0].FadedRating)
 }
--- mswclient/mswclient.go
+++ mswclient/mswclient.go
@@ -4,6 +4,7 @@ import (
   "encoding/json"
   "io/ioutil"
   "net/http"
+  "fmt"
 )
 
 type SpotResults []struct {
@@ -18,6 +19,17 @@ type searchResult []struct {
   Type string `json:"type"`
 }
 
+type ForecastResults []struct {
+  FadedRating int64 `json:"fadedRating"`
+  SolidRating int64 `json:"solidRating"`
+  Swell       struct {
+    Height    float64 `json:"height"`
+    Period    int64   `json:"period"`
+    Unit      string  `json:"unit"`
+  } `json:"swell"`
+  Timestamp int64 `json:"timestamp"`
+}
+
 // GetSpots returns a list of spots matching the given query string
 func GetSpots(query string) SpotResults {
   response, err := http.Get("https://magicseaweed.com/api/mdkey/search?match=CONTAINS&type=SPOT&query=" + query)
@@ -29,9 +41,24 @@ func GetSpots(query string) SpotResults {
 
   data, _ := ioutil.ReadAll(response.Body)
 
-  var result searchResult
+  var results searchResult
+  json.Unmarshal([]byte(data), &results)
 
-  json.Unmarshal([]byte(data), &result)
+  return results[0].SpotResults
+}
 
-  return result[0].SpotResults
+func GetForecast(spotId int64) ForecastResults {
+  var forecastEndpoint = fmt.Sprintf("http://magicseaweed.com/api/mdkey/forecast/?spot_id=%d&fields=solidRating,fadedRating,swell.*,timestamp", spotId)
+  response, err := http.Get(forecastEndpoint)
+
+  if err != nil {
+    panic(err)
+  }
+
+  data, _ := ioutil.ReadAll(response.Body)
+
+  var results ForecastResults
+  json.Unmarshal([]byte(data), &results)
+
+  return results
 }
